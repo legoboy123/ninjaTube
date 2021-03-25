c7cad1ede6c2a995f8fba0f56a6938e470f70aa4
legoboy123 <77392142+legoboy123@users.noreply.github.com>
2021-03-25 21:19:52 +0900

    2.22 DONE - default
    동영상 재생 안됨

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ import app from "./app";
 const PORT = 4000;
 
 const handleListening = () => 
-console.log(`🏮--그림자의 세계가 열렸다...http://localhost:${PORT}--🏮`);
+console.log(`🏮-- 그림자의 세계가 열렸다...http://localhost:${PORT} --🏮`);
 
 
 app.listen(PORT, handleListening);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,8 +4,8 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
