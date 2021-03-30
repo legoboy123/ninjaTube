f4d101e121b5ac2b83f15dc7866d4a593b08b976
legoboy123 <77392142+legoboy123@users.noreply.github.com>
2021-03-30 21:07:02 +0900

    4.2 DONE

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
 const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,9 +22,12 @@ app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
-    return next();
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://archive.org"
+  );
+  return next();
 });
 
 app.use(routes.home, globalRouter);
